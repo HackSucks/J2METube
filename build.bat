d9ef9330f36fa05f48feea63870b340897d14fa2
NVMeorL2S <yadhrmstr@gmail.com>
2026-03-09 16:30:28 +0300

    Update build.bat for 1.1.2 classes

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ echo [1/4] Compiling...
 echo [2/4] Preverifying...
 if exist preverified rd /s /q preverified
 mkdir preverified
-"%SDK_PATH%\bin\preverify.exe" -classpath "%SDK_PATH%\lib\midp_2.1.jar;%SDK_PATH%\lib\cldc_1.1.jar;%SDK_PATH%\lib\jsr135_1.2.jar;%SDK_PATH%\lib\jsr75_1.0.jar" -d preverified J2METube J2METube$1 J2METube$2 J2METube$2$1 J2METube$2$2 J2METube$3
+"%SDK_PATH%\bin\preverify.exe" -classpath "%SDK_PATH%\lib\midp_2.1.jar;%SDK_PATH%\lib\cldc_1.1.jar;%SDK_PATH%\lib\jsr135_1.2.jar;%SDK_PATH%\lib\jsr75_1.0.jar" -d preverified J2METube J2METube$1 J2METube$2  J2METube$3 J2METube$4
 
 echo [3/4] Packaging JAR...
 cd preverified
@@ -21,4 +21,5 @@ for %%I in (J2METube.jar) do set size=%%~zI
 powershell -Command "(gc J2METube.jad) -replace 'MIDlet-Jar-Size:.*', 'MIDlet-Jar-Size: %size%' | Out-File -encoding ascii J2METube.jad"
 
 echo Done!
+
 
